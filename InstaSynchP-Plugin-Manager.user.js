ef2b5d593291dcfb05ea48fba11df5a150feff93
Julian Hangstörfer <jhangstoerfer@gmail.com>
2015-02-02 13:17:07 +0100

    Added InstaSynchP-All

--- InstaSynchP-Plugin-Manager.user.js
+++ InstaSynchP-Plugin-Manager.user.js
@@ -3,7 +3,7 @@
 // @namespace   InstaSynchP
 // @description List plugins, their version, info link and update notifications
 
-// @version     1.1
+// @version     1.1.1
 // @author      Zod-
 // @source      https://github.com/Zod-/InstaSynchP-Plugin-Manager
 // @license     MIT
@@ -337,4 +337,4 @@ PluginManager.prototype.save = function (close, refresh) {
 };
 
 window.plugins = window.plugins || {};
-window.plugins.pluginManager = new PluginManager('1.1');
+window.plugins.pluginManager = new PluginManager('1.1.1');
